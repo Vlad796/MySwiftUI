77490a1d15e1ed21ee1dec4f717b205de195ec3f
Vlad796 <gonevlad02@gmail.com>
2024-12-17 12:44:53 +0300

    Initial Commit

---
+++ MySwiftUI/MySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIApp.swift
+//  MySwiftUI
+//
+//  Created by Влад Варламов on 17.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
