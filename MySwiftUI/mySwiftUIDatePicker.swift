12bc852055db87e1d985b15d7ad71d9d8d3d56d6
Vlad796 <gonevlad02@gmail.com>
2025-01-09 22:31:08 +0300

    Comit09.01.25

---
+++ MySwiftUI/mySwiftUIDatePicker.swift
@@ -0,0 +1,18 @@
+//
+//  mySwiftUIDatePicker.swift
+//  MySwiftUI
+//
+//  Created by Влад Варламов on 09.01.2025.
+//
+
+import SwiftUI
+
+struct mySwiftUIDatePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mySwiftUIDatePicker()
+}
---
+++ MySwiftUI/mySwiftUIPicker.swift
@@ -0,0 +1,18 @@
+//
+//  mySwiftUIPicker.swift
+//  MySwiftUI
+//
+//  Created by Влад Варламов on 09.01.2025.
+//
+
+import SwiftUI
+
+struct mySwiftUIPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mySwiftUIPicker()
+}
